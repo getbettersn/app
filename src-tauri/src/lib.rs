50faef9484d2069f1c352b838f3a97fafbe05e02
sxdny <67187745+sxdny@users.noreply.github.com>
2025-11-14 12:18:41 +0100

    added exit button frontend
    added an "Exit" button on the frontend.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,12 @@
 use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
     tray::{MouseButton, MouseButtonState, TrayIconEvent},
-    Manager, menu::{Menu, MenuItem}, tray::TrayIconBuilder
+    Manager,
 };
 
-use tauri_plugin_positioner::{WindowExt, Position};
+use tauri::Listener;
+use tauri_plugin_positioner::{Position, WindowExt};
 use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -12,54 +15,50 @@ pub fn run() {
         .plugin(tauri_plugin_positioner::init())
         .setup(|app| {
 
+            app.listen("quitProgram", |event| {
+                std::process::exit(0)
+            });
+
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .build(app)?;
-            
+
             let window = app.get_webview_window("main").unwrap();
 
             #[cfg(target_os = "macos")]
             apply_vibrancy(
-                &window, 
-                NSVisualEffectMaterial::Menu, 
-                Some(NSVisualEffectState::Active), 
-                Some(6.0)
-            ).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+                &window,
+                NSVisualEffectMaterial::Menu,
+                Some(NSVisualEffectState::Active),
+                Some(6.0),
+            )
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             Ok(())
-        })
-        .on_menu_event(|app, event| match event.id.as_ref() {
-            "quit" => {
-                app.exit(0);
-            }
-            _ => {
-
-            }
         })
         .on_tray_icon_event(|tray, event| {
             tauri_plugin_positioner::on_tray_event(tray, &event);
             match event {
-            
-            TrayIconEvent::Click {
-                position: _,
-                button: MouseButton::Left,
-                button_state: MouseButtonState::Up,
-                ..
-            } => {
-                let app = tray.app_handle();
-                if let Some(window) = app.get_webview_window("main") {
-                    let _ = window.move_window(Position::TrayCenter);
-                    if window.is_visible().unwrap() {
-                        window.hide().unwrap();
-                    } else {
-                        window.show().unwrap();
-                        window.set_focus().unwrap();
+                TrayIconEvent::Click {
+                    position: _,
+                    button: MouseButton::Left,
+                    button_state: MouseButtonState::Up,
+                    ..
+                } => {
+                    let app = tray.app_handle();
+                    if let Some(window) = app.get_webview_window("main") {
+                        let _ = window.move_window(Position::TrayCenter);
+                        if window.is_visible().unwrap() {
+                            window.hide().unwrap();
+                        } else {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
                     }
                 }
-            }
-            _ => {
-                println!("unhandled event {event:?}")
-            }
+                _ => {
+                    println!("unhandled event {event:?}")
+                }
             }
         })
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { emit } from "@tauri-apps/api/event";
 
 function App() {
+
+  function emitQuit() {
+    emit('quitProgram', {
+      'quit': true
+    })
+  }
 
   return (
     // this should be displayed inside the system tray menu
     <main className="container">
       <h1>Hello system tray</h1>
+      <button onClick={emitQuit}>Exit</button>
     </main>
   );
 }
