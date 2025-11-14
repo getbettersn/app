f858e36bab4f7d6545d83425793b64b0af6dd847
sxdny <67187745+sxdny@users.noreply.github.com>
2025-11-14 11:47:47 +0100

    changes to main window
    - removed decorations
    - position to `TrayIcon::Center` with tauri_positioner
    - quit event and more
    
    todo: move quit event to main window.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,70 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    tray::{MouseButton, MouseButtonState, TrayIconEvent},
+    Manager, menu::{Menu, MenuItem}, tray::TrayIconBuilder
+};
+
+use tauri_plugin_positioner::{WindowExt, Position};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .build(app)?;
+            
+            // let window = app.get_webview_window("main").unwrap();
+
+            // #[cfg(target_os = "macos")]
+            // apply_vibrancy(
+            //     &window, 
+            //     NSVisualEffectMaterial::Menu, 
+            //     Some(NSVisualEffectState::Active), 
+            //     Some(6.0)
+            // ).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            Ok(())
+        })
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                app.exit(0);
+            }
+            _ => {
+
+            }
+        })
+        .on_tray_icon_event(|tray, event| {
+            tauri_plugin_positioner::on_tray_event(tray, &event);
+            match event {
+            
+            TrayIconEvent::Click {
+                position: _,
+                button: MouseButton::Left,
+                // button_state: MouseButtonState::Up,
+                ..
+            } => {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    // TODO: set to tray center. --> set tray positoin
+                    let _ = window.move_window(Position::TrayCenter);
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                }
+            }
+            _ => {
+                println!("unhandled event {event:?}")
+            }
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
