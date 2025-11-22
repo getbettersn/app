389d678a502c24bc8cdbc183ecc1c1d505509f43
sxdny <67187745+sxdny@users.noreply.github.com>
2025-11-22 18:51:21 +0100

    tweaks + local file reading
    now, able to read and write files from the AppLocalData folder.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,74 @@ import "./App.css";
 import { listen, emit } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
 
+import {
+  BaseDirectory,
+  exists,
+  mkdir,
+  writeFile,
+  readDir,
+  DirEntry,
+} from "@tauri-apps/plugin-fs";
+
+interface Folder {
+  name: string;
+  dir: string;
+}
+
 function App() {
-  const [markdown, setMarkdown] = useState("Nothing");
+  const [notesFolder, setNotesFolder] = useState<Folder>({
+    name: "notes",
+    dir: "notes",
+  });
+
+  const [message, setMessage] = useState("");
+  const [files, setFiles] = useState<DirEntry[]>([]);
 
   useEffect(() => {
-    console.log("Executed")
-    listen<string>('ready', (event) => {
-    console.log("The page is ready")
-  }) });
-  
+    checkNotesFolderExists();
+    readFiles();
+  }, []);
+
+  async function createNotesFolder() {
+    try {
+      await mkdir(notesFolder.name, {
+        baseDir: BaseDirectory.AppLocalData,
+      });
+    } catch (error: any) {
+      console.error(error);
+      setMessage(error);
+    }
+  }
+
+  async function checkNotesFolderExists() {
+    try {
+      let doesItExist = await exists(notesFolder.name, {
+        baseDir: BaseDirectory.AppLocalData,
+      });
+
+      if (doesItExist) {
+        setMessage("The notes folder exists. No need to create one.");
+      } else {
+        setMessage("The notes folder does not exist...");
+        createNotesFolder();
+      }
+    } catch (error: any) {
+      console.error(error);
+      setMessage(error);
+    }
+  }
+
+  async function readFiles() {
+    try {
+      const files = await readDir(notesFolder.name, {
+        baseDir: BaseDirectory.AppLocalData,
+      });
+      setFiles(files);
+      console.log(files);
+    } catch (error: any) {
+      console.error(error);
+    }
+  }
 
   function emitQuit() {
     emit("quitProgram", {
@@ -21,7 +80,11 @@ function App() {
   return (
     // this should be displayed inside the system tray menu
     <main className="container">
-      <h1>Hello system tray {markdown} </h1>
+      <h1>Hello World!</h1>
+      <p>{message}</p>
+      {files.map((file) => (
+        <p> {file.name}</p>
+      ))}
       <button onClick={emitQuit}>Exit</button>
     </main>
   );
